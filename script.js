44da40d87b2004be0a53172709a1ddd69c599924
Joey Imlay <joanne.imlay@ladbiblegroup.com>
2021-03-17 19:20:33 +0000

    updated README

--- script.js
+++ script.js
@@ -2,8 +2,3 @@ function onClick() {
   var thisItem = event.target.parentNode;
   thisItem.classList.toggle("open");
 };
-
-// function onClickMobile() {
-//   var thisItem = event.target;
-//   thisItem.classList.toggle("open");
-// };
