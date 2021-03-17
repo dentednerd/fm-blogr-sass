56b20aab23de5c6ce9e43c1d472c25e046a0787c
Joey Imlay <joanne.imlay@ladbiblegroup.com>
2021-03-17 19:02:03 +0000

    implemented Sass modules and mobile design

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function onClick() {
   var thisItem = event.target.parentNode;
   thisItem.classList.toggle("open");
 };
+
+// function onClickMobile() {
+//   var thisItem = event.target;
+//   thisItem.classList.toggle("open");
+// };
